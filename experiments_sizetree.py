30e899a7151ffe091b9acc34be4e1767cf385bee
Lanfang <875387859@qq.com>
2023-07-03 15:11:57 +0200

    add reg

--- experiments_sizetree.py
+++ experiments_sizetree.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.metrics import average_precision_score
 from sklearn.ensemble import IsolationForest
 from size_tree.sizeRegressor_sorted import *
+from sklearn.tree import DecisionTreeRegressor
 
 if __name__ == '__main__':
     print('Using incremental algorithm!!!')
@@ -18,14 +19,16 @@ if __name__ == '__main__':
     X.columns = [int(x) for x in X.columns]
     print('~~~~~~~~~~~~~~~~~~~~~~dataset: ', dataset)
     print('n=', len(y), ' d=', len(X.columns), ' anomaly=', sample_size)
-    
+
     black_ap = np.zeros(10)
     size_ap = np.zeros(10)
     counttime = 0
+    reg_ap = np.zeros(10)
     for i in range(10):
         black_box = IsolationForest()
         black_box.fit(X)
-        anomalyscores = 0.5 - black_box.decision_function(X)
+        if_preds = black_box.decision_function(X)
+        anomalyscores = 0.5 - if_preds
         black_ap[i] = average_precision_score(y, anomalyscores)
         import time
         starttime = time.time()
@@ -35,8 +38,12 @@ if __name__ == '__main__':
         explainer_predict = explainer.predict_train()
         size_ap[i] = average_precision_score(y, explainer_predict)
 
-    print('IF ap: %.3f, Our ap: %.3f, time per run: %.3f s'%(black_ap.mean(), size_ap.mean(), counttime/10))
-    
+        explainer = DecisionTreeRegressor(max_depth=5)
+        explainer.fit(X, anomalyscores)
+        reg_scores = explainer.predict(X)
+        reg_ap[i] = average_precision_score(y,reg_scores)
+
+    print('IF ap: %.3f, Our ap: %.3f, CART ap: %.3f, time per run: %.3f s'%(black_ap.mean(), reg_ap.mean(), size_ap.mean(), counttime/10))
 
 
 
